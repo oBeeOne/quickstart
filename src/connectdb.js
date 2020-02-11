3f28f5f3aab4496014a87736ce659e477d2fa295
oBeeOne <olivier@digiarts-solutions.net>
2020-02-12 00:09:46 +0100

    Create dockerizing files

--- src/connectdb.js
+++ src/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const User = ("./User.model");
-const connection = "mongodb://localhost:27017/quick-start";
+const connection = "mongodb://mongo:27017/quick-start";
 const connectDatabase = () =>{
     return mongoose.connect(connection);
 };
