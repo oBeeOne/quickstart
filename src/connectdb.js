2078ba9e3945ba92b51ea4e2aa38034c7f56d563
oBeeOne <olivier@digiarts-solutions.net>
2020-02-12 11:50:00 +0100

    Update connectdb.js

--- src/connectdb.js
+++ src/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const User = ("./User.model");
-const connection = "mongodb://mongo:27017/quick-start";
+const connection = "mongodb://192.168.99.100:27017/quick-start";
 const connectDatabase = () =>{
     return mongoose.connect(connection);
 };
